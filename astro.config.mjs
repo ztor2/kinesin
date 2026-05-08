fb1df60f6bde2cd6e124d84f6f0ee5cb90db85c9
ztor2 <ztor2k@gmail.com>
2026-05-08 13:50:07 +0900

    ui improvements - 1

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,9 +8,6 @@ export default defineConfig({
 			title: 'Kinesin',
 			favicon: '/favicon.ico',
 			lastUpdated: true,
-			social: [
-				{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' },
-			],
 			customCss: [
 				'./src/styles/custom.css',
 			],
@@ -20,12 +17,8 @@ export default defineConfig({
 			},
 			sidebar: [
 				{
-					label: '용어 사전',
-					collapsed: true, // 접혀있는 상태로 시작
-					autogenerate: { directory: 'dictionary' },
+					autogenerate: { directory: '' },
 				},
-				// 새로운 카테고리가 필요하면 여기에 추가하세요.
-
 			],
 		}),
 	],
