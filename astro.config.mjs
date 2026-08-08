90594b53f5ffed7bb178aa459bfc7f95dd9054b3
ztor2 <ztor2k@gmail.com>
2026-08-08 20:07:22 +0900

    tmp

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,19 +22,11 @@ export default defineConfig({
       head: [
         {
           tag: 'link',
-          attrs: { rel: 'preconnect', href: 'https://cdn.jsdelivr.net', crossorigin: 'anonymous' }
+          attrs: { rel: 'preload', href: '/fonts/GmarketSansTTFMedium.woff2', as: 'font', type: 'font/woff2', crossorigin: 'anonymous' }
         },
         {
           tag: 'link',
-          attrs: { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: 'anonymous' }
-        },
-        {
-          tag: 'link',
-          attrs: { rel: 'preload', href: 'https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2001@1.1/GmarketSansMedium.woff', as: 'font', type: 'font/woff', crossorigin: 'anonymous' }
-        },
-        {
-          tag: 'link',
-          attrs: { rel: 'preload', href: 'https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2001@1.1/GmarketSansBold.woff', as: 'font', type: 'font/woff', crossorigin: 'anonymous' }
+          attrs: { rel: 'preload', href: '/fonts/GmarketSansTTFBold.woff2', as: 'font', type: 'font/woff2', crossorigin: 'anonymous' }
         }
       ],
       components: {
